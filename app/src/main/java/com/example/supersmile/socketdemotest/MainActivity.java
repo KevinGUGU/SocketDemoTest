0a945b59dee6fb000796257e0de6491e59c1fcbc
KevinGUGU <362074193@qq.com>
2017-04-15 20:54:30 +0800

    //点击按键，更新显示内容

--- app/src/main/java/com/example/supersmile/socketdemotest/MainActivity.java
+++ app/src/main/java/com/example/supersmile/socketdemotest/MainActivity.java
@@ -2,14 +2,28 @@ package com.example.supersmile.socketdemotest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        textView = (TextView) findViewById(R.id.tv_desk);
+        button = (Button) findViewById(R.id.bt_click);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("哈哈 我来了！");
+            }
+        });
     }
 }
